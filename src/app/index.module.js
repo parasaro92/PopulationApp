159b34f0916d728a4eae576f6353e20cc6a97e79
sundarcodes <sundar82@gmail.com>
2016-06-12 14:15:37 +0530

    Make it responsive

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,6 +4,6 @@
   angular
     .module('populationApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 
       'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 
-      'toastr','highcharts-ng']);
+      'highcharts-ng']);
 
 })();
